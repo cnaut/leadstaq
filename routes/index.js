70ee77dbec0dd5471b0e73a7cc03e0003066cd2e
Charles Naut <cpn585@gmail.com>
2014-02-26 23:27:48 -0800

    Hook up basic twitter integration and hard cord tweet search

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
+var Twit = require('twit');
+
+console.log(process.env.TWITTER_API_KEY)
+
+var Twitter = new Twit({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  Twitter.get('search/tweets', { q: "startups", count: 100 }, function(err, reply) {
+  res.render('index', { tweets: reply });
+  });
 };
