0ce891ab795eebcb5be1eafd6d1904f3c85e41c3
Charles Naut <cpn585@gmail.com>
2014-02-27 13:49:54 -0800

    Add ability to click on button corresponding to tweet to add user who tweeted to add list

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,19 @@
-$("#search-btn").click(function() {
-  console.log($("#search-box").val());
-  $.ajax({
-    url: "/twittersearch/" + $("#search-box").val(),
-    success: function(results) {
-      document.getElementById("tweets").innerHTML = new EJS({ url: 'ejs/tweets.ejs' }).render({ tweets: results.statuses });
-    }
+$(function() {
+  $("#search-btn").click(function() {
+    $.ajax({
+      url: "/twittersearch/" + $("#search-box").val(),
+      success: function(results) {
+        document.getElementById("tweets").innerHTML = new EJS({ url: 'ejs/tweets.ejs' }).render({ tweets: results.statuses });
+      }
+    });
+  });
+
+  $("#tweets").on('click', ".add-btn", function() {
+    var tweetDiv = $(this).parent().parent();
+    tweetDiv.hide();
+    var name = tweetDiv.find(".username").html();
+    var handle = tweetDiv.find(".username").attr("href");
+    var description = tweetDiv.find(".description").html();
+    $("#added").append("<tr><td>" + name + "</td><td>" + handle + "</td><td>" + description + "</td></tr>");
   });
 });
